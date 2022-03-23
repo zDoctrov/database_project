b416b4bdaf5b8f5318d594440fde5fedbb0922db
Abdallah99GIT <71734598+Abdallah99GIT@users.noreply.github.com>
2022-03-23 18:30:18 -0400

    DataBasehandler.kt file

---
+++ app/src/main/java/com/example/database_project/DataBasehandler.kt
@@ -0,0 +1,14 @@
+package com.example.database_project
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+
+
+
+
