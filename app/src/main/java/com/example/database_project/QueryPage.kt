dcfdfe4ba2dea7459607b0d4a8dc2d17a53a5d17
Luke Beauchamp <lukebeau@umich.edu>
2022-03-30 18:39:17 -0400

    Add query page

---
+++ app/src/main/java/com/example/database_project/QueryPage.kt
@@ -0,0 +1,11 @@
+package com.example.database_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QueryPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_query_page)
+    }
+}
