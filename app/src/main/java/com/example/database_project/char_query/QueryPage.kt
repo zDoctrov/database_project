808ceaec101809a03ca8839936d80b6abc47983d
Abdallah99GIT <71734598+Abdallah99GIT@users.noreply.github.com>
2022-04-06 20:00:55 -0400

    Changes of spinner for the query page is working now :)

--- app/src/main/java/com/example/database_project/char_query/QueryPage.kt
+++ app/src/main/java/com/example/database_project/char_query/QueryPage.kt
@@ -24,9 +24,32 @@ class QueryPage : AppCompatActivity() {
         setContentView(binding.root)
 
         //Setup ArrayAdapters for the data used inside the spinners
-        val raceList = res.getStringArray(R.array.races)
+        val raceList = res.getStringArray(R.array.races_empty)
         val race2ArrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, raceList)
         binding.spinnerRace2.adapter = race2ArrayAdapter
+
+        //Setup ArrayAdapters for the data used inside the spinners
+        val factionList = res.getStringArray(R.array.factions_empty)
+        val faction2ArrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, factionList)
+        binding.spinnerFaction2.adapter = faction2ArrayAdapter
+
+
+        val hairList = res.getStringArray(R.array.hair_empty)
+        val hair2ArrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, hairList)
+        binding.spinnerHair2.adapter = hair2ArrayAdapter
+
+        val earsList = res.getStringArray(R.array.ears_empty)
+        val ears2ArrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, earsList)
+        binding.spinnerEars2.adapter = ears2ArrayAdapter
+
+        val eyesList = res.getStringArray(R.array.eyes_empty)
+        val eyes2ArrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, eyesList)
+        binding.spinnerEyes2.adapter = eyes2ArrayAdapter
+
+        val classList = res.getStringArray(R.array.user_class_empty)
+        val class2ArrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, classList)
+        binding.spinnerClass2.adapter = class2ArrayAdapter
+
 
 
 
