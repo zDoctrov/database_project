e4a40d610ef3c9a709545d5dac633bcebb5dc0ab
Luke Beauchamp <lukebeau@umich.edu>
2022-04-13 21:36:36 -0400

    layered images on InstanceResultPage.kt and QueryCharEditPage.kt

--- app/src/main/java/com/example/database_project/char_query/InstanceResultPage.kt
+++ app/src/main/java/com/example/database_project/char_query/InstanceResultPage.kt
@@ -2,8 +2,10 @@ package com.example.database_project.char_query
 
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AlertDialog
 import com.example.database_project.MainActivity
 import com.example.database_project.R
@@ -18,10 +20,102 @@ class InstanceResultPage() : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_instance_result_page)
+        val res: Resources = resources
 
         //Set up binding, so you can reference the ID's of things on this activity's layout
         binding = ActivityInstanceResultPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        //for layering the images
+        var imageNameRace = "@drawable/"
+        var imageNameHair = "@drawable/"
+        var imageNameEyes = "@drawable/"
+        var imageNameEars = "@drawable/"
+
+        val positionRace = intent.getIntExtra("position", -1)
+        val raceType = creationSession.queryResults[positionRace].race
+
+        val positionHair = intent.getIntExtra("position", -1)
+        val HairType = creationSession.queryResults[positionHair].hair
+
+        val positionEyes = intent.getIntExtra("position", -1)
+        val EyeType = creationSession.queryResults[positionEyes].eyes
+
+        val positionEars = intent.getIntExtra("position", -1)
+        val EarType = creationSession.queryResults[positionEars].ears
+
+        when(raceType){
+            "Human"->{
+                imageNameRace = "@drawable/race_human"
+            }
+            "Dwarf"->{
+                imageNameRace = "@drawable/race_dwarf"
+            }
+            "Elf"->{
+                imageNameRace = "@drawable/race_elf"
+            }
+        }
+
+        when(HairType){
+            "Long"->{
+                imageNameHair = "@drawable/hair_long"
+            }
+            "Short"->{
+                imageNameHair = "@drawable/hair_short"
+            }
+            "Fuzzy"->{
+                imageNameHair = "@drawable/hair_fuzzy"
+            }
+        }
+
+        when(EyeType){
+            "Round"->{
+                imageNameEyes = "@drawable/eyes_round"
+            }
+            "Squinted"->{
+                imageNameEyes = "@drawable/eyes_squinted"
+            }
+            "Lazy"->{
+                imageNameEyes = "@drawable/eyes_lazy"
+            }
+        }
+
+        when(EarType){
+            "Round"->{
+                imageNameEars = "@drawable/ears_round"
+            }
+            "Pointed"->{
+                imageNameEars = "@drawable/ears_pointed"
+            }
+            "Square"->{
+                imageNameEars = "@drawable/ears_square"
+            }
+        }
+
+        var imageResId1= resources.getIdentifier(imageNameRace, "drawable", packageName)
+        var imageResId2= resources.getIdentifier(imageNameHair, "drawable", packageName)
+        var imageResId3 = resources.getIdentifier(imageNameEyes, "drawable", packageName)
+        var imageResId4 = resources.getIdentifier(imageNameEars, "drawable", packageName)
+
+        binding.imageView4.layoutParams = LinearLayout.LayoutParams(600, 900)
+        binding.imageView4.x = 230F
+        binding.imageView4.y = -150F
+        binding.imageView11.layoutParams = LinearLayout.LayoutParams(600, 900)
+        binding.imageView11.x = 230F
+        binding.imageView11.y = -150F
+        binding.imageView12.layoutParams = LinearLayout.LayoutParams(600, 900)
+        binding.imageView12.x = 230F
+        binding.imageView12.y = -150F
+        binding.imageView13.layoutParams = LinearLayout.LayoutParams(600, 900)
+        binding.imageView13.x = 230F
+        binding.imageView13.y = -150F
+
+        binding.imageView4.setImageResource(imageResId1)
+        binding.imageView13.setImageResource(imageResId2)
+        binding.imageView12.setImageResource(imageResId3)
+        binding.imageView11.setImageResource(imageResId4)
+
 
         //Get which card cell was chosen and apply their data to the activity screen as appropriately
         val position = intent.getIntExtra("position", -1)
--- app/src/main/java/com/example/database_project/char_query/QueryCharEditPage.kt
+++ app/src/main/java/com/example/database_project/char_query/QueryCharEditPage.kt
@@ -2,13 +2,17 @@ package com.example.database_project.char_query
 
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.provider.SyncStateContract.Helpers.update
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Update
+import com.example.database_project.MainActivity
 import com.example.database_project.R
 import com.example.database_project.char_creator.creationSession
+import com.example.database_project.databinding.ActivityInstanceResultPageBinding
 import com.example.database_project.databinding.QueryCharEditPageBinding
 import com.example.database_project.room_db.RoomAppDB
 
@@ -22,11 +26,104 @@ class QueryCharEditPage: AppCompatActivity() {
 
         setContentView(R.layout.query_char_edit_page)
 
+        binding = QueryCharEditPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //for layering the images
+        var imageNameRace = "@drawable/"
+        var imageNameHair = "@drawable/"
+        var imageNameEyes = "@drawable/"
+        var imageNameEars = "@drawable/"
+
+        val positionRace = intent.getIntExtra("position", -1)
+        val raceType = creationSession.queryResults[positionRace].race
+
+        val positionHair = intent.getIntExtra("position", -1)
+        val HairType = creationSession.queryResults[positionHair].hair
+
+        val positionEyes = intent.getIntExtra("position", -1)
+        val EyeType = creationSession.queryResults[positionEyes].eyes
+
+        val positionEars = intent.getIntExtra("position", -1)
+        val EarType = creationSession.queryResults[positionEars].ears
+
+        when(raceType){
+            "Human"->{
+                imageNameRace = "@drawable/race_human"
+            }
+            "Dwarf"->{
+                imageNameRace = "@drawable/race_dwarf"
+            }
+            "Elf"->{
+                imageNameRace = "@drawable/race_elf"
+            }
+        }
+
+        when(HairType){
+            "Long"->{
+                imageNameHair = "@drawable/hair_long"
+            }
+            "Short"->{
+                imageNameHair = "@drawable/hair_short"
+            }
+            "Fuzzy"->{
+                imageNameHair = "@drawable/hair_fuzzy"
+            }
+        }
+
+        when(EyeType){
+            "Round"->{
+                imageNameEyes = "@drawable/eyes_round"
+            }
+            "Squinted"->{
+                imageNameEyes = "@drawable/eyes_squinted"
+            }
+            "Lazy"->{
+                imageNameEyes = "@drawable/eyes_lazy"
+            }
+        }
+
+        when(EarType){
+            "Round"->{
+                imageNameEars = "@drawable/ears_round"
+            }
+            "Pointed"->{
+                imageNameEars = "@drawable/ears_pointed"
+            }
+            "Square"->{
+                imageNameEars = "@drawable/ears_square"
+            }
+        }
+
+        var imageResId1= resources.getIdentifier(imageNameRace, "drawable", packageName)
+        var imageResId2= resources.getIdentifier(imageNameHair, "drawable", packageName)
+        var imageResId3 = resources.getIdentifier(imageNameEyes, "drawable", packageName)
+        var imageResId4 = resources.getIdentifier(imageNameEars, "drawable", packageName)
+
+        binding.imageView5.layoutParams = LinearLayout.LayoutParams(600, 900)
+        binding.imageView5.x = 230F
+        binding.imageView5.y = -150F
+        binding.imageView14.layoutParams = LinearLayout.LayoutParams(600, 900)
+        binding.imageView14.x = 230F
+        binding.imageView14.y = -150F
+        binding.imageView15.layoutParams = LinearLayout.LayoutParams(600, 900)
+        binding.imageView15.x = 230F
+        binding.imageView15.y = -150F
+        binding.imageView16.layoutParams = LinearLayout.LayoutParams(600, 900)
+        binding.imageView16.x = 230F
+        binding.imageView16.y = -150F
+
+        binding.imageView5.setImageResource(imageResId1)
+        binding.imageView16.setImageResource(imageResId2)
+        binding.imageView15.setImageResource(imageResId3)
+        binding.imageView14.setImageResource(imageResId4)
+
+
         //Get which card cell was chosen and apply their data to the activity screen as appropriately
         val position = intent.getIntExtra("position", -1)
 
-        binding = QueryCharEditPageBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        /*binding = QueryCharEditPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)*/
 
         val saveButton = findViewById<Button>(R.id.btnSave)
         saveButton.setOnClickListener{
@@ -36,7 +133,8 @@ class QueryCharEditPage: AppCompatActivity() {
 
         val homeButton = findViewById<Button>(R.id.btnHome2)
         homeButton.setOnClickListener {
-            finish()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
 
